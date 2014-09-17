62eeee55a4659a5b8d8055745f55bafd5557657e
Subin George <SubinGeorge@users.noreply.github.com>
2014-09-17 21:16:54 +0530

    update repeated_timer by subin 17Sep2014_21:13

--- repeated_timer.py
+++ repeated_timer.py
@@ -9,20 +9,21 @@ class repeatedTimer(object):
         self.function   = function
         self.args       = args
         self.kwargs     = kwargs
-        self.is_running = False
-        self.start()
+        #self.is_running = False
+        #self.start()
 
     def _run(self):
-        self.is_running = False
-        self.start()
+       # self.is_running = False
+        
         self.function(*self.args, **self.kwargs)
-
+        self.start()
+        
     def start(self):
         if not self.is_running:
             self._timer = Timer(self.interval, self._run)
             self._timer.start()
-            self.is_running = True
+          #  self.is_running = True
 
     def stop(self):
         self._timer.cancel()
-        self.is_running = False
+       # self.is_running = False
