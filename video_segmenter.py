4b900905488929fab94d6d0ea10913cdadab7978
Subin George <SubinGeorge@users.noreply.github.com>
2014-09-23 17:33:44 +0530

    video_segmenter by Subin on 23Sep2014_17:33

--- video_segmenter.py
+++ video_segmenter.py
@@ -7,21 +7,29 @@ from Tkinter import *
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
+tol = 20
+
 def VideoPlot():
-	global frame_dup, red_min, red_max, green_min, green_max, blue_min, blue_max
+	global frame_dup1,frame_dup2, red_min, red_max, green_min, green_max, blue_min, blue_max
 	frame1 = video.read(0)[1]
-	frame_dup[:,:,0] = (frame1[:,:,0] <= red_max)*frame1[:,:,0]
-	frame_dup[:,:,0] = (frame_dup[:,:,0] >= red_min)*1
-	frame_dup[:,:,1] = (frame1[:,:,1] <= green_max)*frame1[:,:,1]
-	frame_dup[:,:,1] = (frame_dup[:,:,1] >= green_min)*1
-	frame_dup[:,:,2] = (frame1[:,:,2] <= blue_max)*frame1[:,:,2]
-	frame_dup[:,:,2] = (frame_dup[:,:,2] >= blue_min)*1 
-	frame_dup1 = frame_dup[:,:,0]+frame_dup[:,:,1]+frame_dup[:,:,2]
-	frame_dup1 = frame_dup1==3	
+	frame_dup1[:,:,0] = (frame1[:,:,0] <= red_max)
+	frame_dup2[:,:,0] = (frame1[:,:,0] >= red_min)
+	frame_bin1 = frame1[:,:,0]*frame_dup2[:,:,0]
+	frame_dup1[:,:,1] = (frame1[:,:,1] <= green_max)
+	frame_dup2[:,:,1] = (frame1[:,:,1] >= green_min)
+	frame_bin2 = frame_dup1[:,:,1]*frame_dup2[:,:,1]
+	frame_dup1[:,:,2] = (frame1[:,:,2] <= blue_max)
+	frame_dup2[:,:,2] = (frame1[:,:,2] >= blue_min) 
+	frame_bin3 = frame_dup1[:,:,2]*frame_dup2[:,:,2]
+	frame_dup_bin = frame_bin1*frame_bin2*frame_bin3
+	frame_dup_bin = frame_dup_bin>1
+	#~ print numpy.min(frame_dup_bin)
+	#~ print numpy.max(frame_dup_bin)
+	#~ frame_dup1 = frame_dup1==3	
 	
 	#~ plt.imshow(frame_dup1)
 	#~ plt.show()
-	axes1.imshow(frame_dup1)
+	axes1.imshow(frame_dup_bin)
 	canvas1.draw()
     
 def StartButton():
@@ -38,13 +46,14 @@ def QuitButton():
     quit()
 
 thresh = numpy.loadtxt('Thresh.txt')
-redMean, greenMean, blueMean = thresh
-red_min, red_max = redMean-5, redMean+5
-green_min, green_max = greenMean-5, greenMean+5
-blue_min, blue_max = blueMean-5, blueMean+5
+redMean, greenMean, blueMean, length, width,color = thresh
+red_min, red_max = redMean-tol, redMean+tol
+green_min, green_max = greenMean-tol, greenMean+tol
+blue_min, blue_max = blueMean-tol, blueMean+tol
 #~ print thresh   
 video = cv2.VideoCapture(0)
-frame_dup = video.read(0)[1]
+frame_dup1 = numpy.zeros((length, width,color))
+frame_dup2 = numpy.zeros((length, width,color))
 window = Tk()
 window.geometry('850x7000')
 window.title('Test gui')
@@ -68,3 +77,5 @@ timer1 = repeatedTimer(.1, VideoPlot)
 
 # main loop
 window.mainloop()
+
+
