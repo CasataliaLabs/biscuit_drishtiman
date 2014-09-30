14b88a3626a1294d96b2fbb7df522c79de2d3a41
SubinGeorge <subin@castalialabs.com>
2014-09-30 20:13:15 +0530

    Subin on 30 Sep 2014_20:00

--- color_picker.py
+++ color_picker.py
@@ -1,60 +1,28 @@
 import cv2
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
-from repeated_timer import repeatedTimer
 from Tkinter import *
-from tkMessageBox import *
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
-import sys
+import time
 
-video = cv2.VideoCapture(0)
+if not 'video' in locals():
+	video = cv2.VideoCapture(0)
+if not video.isOpened():
+	video.open(0)
 ret, frame = video.read()
+if ret == False:
+	print 'Check if camera is connected'
+	sys.exit()
+rgbFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+plt.imshow(rgbFrame)
+coordinates = []
+coordinates = np.floor(plt.ginput(n=5, timeout=30, show_clicks=True, mouse_add=1, mouse_pop=3, mouse_stop=2))
+xyCordinates = coordinates.astype('uint8')
+hsvFrame = cv2.cvtColor(rgbFrame, cv2.COLOR_RGB2HSV)
+hsvValues = hsvFrame[xyCordinates[:,0],xyCordinates[:,1]]
+hue,saturation = hsvValues[:,0],hsvValues[:,1]
 
-window = Tk()
-window.title('Biscuit frame')
-window.geometry('500x400')
-
-#Reference Plot
-ReferenceFigure = Figure(figsize=(5, 4), dpi=80)
-axis1 = ReferenceFigure.add_subplot(111)
-axis1.set_title('REFERENCE FRAME')
-canvas1 = FigureCanvasTkAgg(ReferenceFigure, master=window)
-widget=canvas1.get_tk_widget().place(x=10, y=10)
-axis1.imshow(frame)
-
-#initializing variables
-count=0
-red=[]
-green=[]
-blue=[]
-frameShape = frame.shape
-print frameShape
-def click(event):
-	print 'click event'
-	global count,red,green,blue
-	
-	Red,Green,Blue=frame[event.x,event.y]
-	red.append(Red)
-	green.append(Green)
-	blue.append(Blue)
-	Red,Green,Blue=frame[event.x,event.y]
-	print Red, Green, Blue
-	count=count+1
-	print count
-	if count>15:
-		print count
-		threshArray = numpy.array([numpy.mean(red), numpy.mean(green),numpy.mean(blue), frameShape[0],frameShape[1], frameShape[2]])
-		numpy.savetxt('Thresh.txt',threshArray,fmt="%s")
-		#~ quit()
-		sys.exit(0)
-		#~ breeak()	
-
-canvas1.mpl_connect('button_press_event', click)
-
-video.release()
-cv2.destroyAllWindows()
-window.mainloop()
-
-
-
+thresholdArray = np.array([np.min(hue), np.max(hue), np.min(saturation), np.max(saturation)])
+np.savetxt('Threshold.txt',thresholdArray,fmt="%s")
+print  thresholdArray
--- video_segmenter.py
+++ video_segmenter.py
@@ -1,69 +1,57 @@
-
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import time
+# import v4l_module
 from Tkinter import *
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
 from tkintertable.Tables import TableCanvas
 from tkintertable.TableModels import TableModel
-import time
-import sys
+import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 plt.ion()
-tol =	50
 #Function for starting timers
 def StartButton():
     timerFrameDisplay.start()
-
 #Function for stopping timers
 def StopButton():
     timerFrameDisplay.stop()
-    
 def QuitButton():
 	video.release()
 	print 'no quit \n please close'
-	#~ sys.exit(0)
-
 #Function to display video frame,mean of frame variations,display in a table
 def showFrame(hImshow):
-	tic = time.time()
-	global frameDup, thresh
-	frameDup1 = frameDup2 = frameDup
+	hueMin, hueMax, saturationMin, saturationMax = threshold
 	ret,frame = video.read()
 	if ret == None:
 		print "reading from cam failed"
 		return
-	frameDuplicate = frame
-	for i in range(0,3):
-		frameDup1[:,:,i] = (frame[:,:,i] <= thresh[i]+tol)
-		frameDup2[:,:,i] = (frame[:,:,i] >= thresh[i]-tol)
-		frameDup[:,:,i] = frameDup1[:,:,i]*frameDup2[:,:,i]
-	frameMask = frameDup[:,:,0]*frameDup[:,:,1]*frameDup[:,:,2]
-	frameDuplicate[:,:,0]=frame[:,:,0]*frameMask
-	frameDuplicate[:,:,1]=frame[:,:,1]*frameMask
-	frameDuplicate[:,:,2]=frame[:,:,2]*frameMask
-	hImshow.set_array(frameDuplicate)
+	ret, frame = video.read()
+	frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	h,s,v=cv2.split(frameHSV)
+	minimumValue = np.array([hueMin, saturationMin, 0], np.uint8)
+	maximumValue = np.array([hueMax, saturationMax , 255], np.uint8)
+	biscuit = cv2.inRange(frameHSV, minimumValue, maximumValue)
+	biscuit = biscuit==0
+	biscuit=biscuit*255
+	#~ print biscuit
+	hImshow.set_array(biscuit)
 	canvas1.show()
 	toc = time.time()
-	
-	data = {'1': {'Time': '{0:.3f}'.format(toc - tic)}}
+	'''data = {'1': {'Time': '{0:.3f}'.format(toc - tic)}}
 	model = table.model
 	model.importDict(data)
-	table.redrawTable()
-	#~ print (toc - tic)
-	
+	table.redrawTable()'''
 print 'start'	
 guiMain = Tk()
 guiMain.geometry('850x7000')
 guiMain.title('biscuit_inspection') #how to set full screen
 print 'gui created'
-	
-	
 #Capturing video frame
 if not 'video' in locals():
-	video = cv2.VideoCapture(0)
+	video = cv2.VideoCapture()
 while not video.isOpened():
 	print 'attempting open video'
 	video.open(0)
@@ -71,35 +59,28 @@ while not video.isOpened():
 else:
 	print "video already open"
 ret, frame = video.read()
+frameRgb = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
 print "frame acquired status", ret
-thresh = np.loadtxt('Thresh.txt')
-redMean, greenMean, blueMean, length, width,color = thresh
-frameDup = np.zeros((length, width,color))
-
+threshold = np.loadtxt('Threshold.txt')
 #Creating figure for showing video frame
 videoFigure = Figure(figsize=(5, 5))
 axes1 = videoFigure.add_subplot(111)
 videoFigure.suptitle("Live Video")
-hImshow = axes1.imshow(frame,cmap = cm.Greys_r, vmin=20, vmax=80)
-
+hImshow = axes1.imshow(frameRgb, cmap = cm.Greys)
 #Creating canvass for placing videou frame
 canvas1 = FigureCanvasTkAgg(videoFigure, master=guiMain)
 canvas1.get_tk_widget().place(x=10, y=20)
-
-#~ # table creation
-frameValue = Frame(guiMain)
+# table creation
+'''frameValue = Frame(guiMain)
 frameValue.pack()
 frameValue.place(x=100, y=500,width=300, height=70)
 model = TableModel()
 table = TableCanvas(frameValue, model=model, editable=False)
-table.createTableFrame()
-	
+table.createTableFrame()'''
 #Timer for frame
 timerFrameDisplay = videoFigure.canvas.new_timer(interval=10)
 timerFrameDisplay.add_callback(showFrame, hImshow)
-
 #Start and Stop Buttons
 button1 = Button(guiMain, text="Start", bg='white', command=StartButton).place(x=50, y=600)
 button2 = Button(guiMain, text="Stop", bg='white', command=StopButton).place(x=200, y=600)
 button3 = Button(guiMain, text="Quit", bg='white', command=QuitButton).place(x=400, y=600)
-
