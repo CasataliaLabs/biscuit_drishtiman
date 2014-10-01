2b34d4c55fcf558c8e639698774617ca2b4d28cc
Subin George <subin@WorkSpace.(none)>
2014-10-01 15:34:36 +0530

    Subin: workable video segmenter

--- color_picker.py
+++ color_picker.py
@@ -15,11 +15,12 @@ if ret == False:
 	print 'Check if camera is connected'
 	sys.exit()
 rgbFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-plt.imshow(rgbFrame)
+hsvFrame = cv2.cvtColor(rgbFrame, cv2.COLOR_RGB2HSV)
+plt.imshow(rgbFrame[:,:,1])
 coordinates = []
 coordinates = np.floor(plt.ginput(n=5, timeout=30, show_clicks=True, mouse_add=1, mouse_pop=3, mouse_stop=2))
 xyCordinates = coordinates.astype('uint8')
-hsvFrame = cv2.cvtColor(rgbFrame, cv2.COLOR_RGB2HSV)
+
 hsvValues = hsvFrame[xyCordinates[:,0],xyCordinates[:,1]]
 hue,saturation = hsvValues[:,0],hsvValues[:,1]
 
--- video_segmenter.py
+++ video_segmenter.py
@@ -1,8 +1,5 @@
-import cv2
+import cv2,sys,time
 import numpy as np
-import sys
-import time
-# import v4l_module
 from Tkinter import *
 from tkintertable.Tables import TableCanvas
 from tkintertable.TableModels import TableModel
@@ -11,72 +8,55 @@ import matplotlib.cm as cm
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 plt.ion()
-#Function for starting timers
+
 def StartButton():
     timerFrameDisplay.start()
-#Function for stopping timers
 def StopButton():
     timerFrameDisplay.stop()
 def QuitButton():
 	video.release()
-	print 'no quit \n please close'
-#Function to display video frame,mean of frame variations,display in a table
+	
 def showFrame(hImshow):
+	tic=time.time()
+	threshold = [2, 40, 100, 250]
 	hueMin, hueMax, saturationMin, saturationMax = threshold
 	ret,frame = video.read()
-	if ret == None:
-		print "reading from cam failed"
-		return
-	ret, frame = video.read()
 	frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-	frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-	h,s,v=cv2.split(frameHSV)
 	minimumValue = np.array([hueMin, saturationMin, 0], np.uint8)
 	maximumValue = np.array([hueMax, saturationMax , 255], np.uint8)
 	biscuit = cv2.inRange(frameHSV, minimumValue, maximumValue)
-	biscuit = biscuit==0
-	biscuit=biscuit*255
-	#~ print biscuit
-	hImshow.set_array(biscuit)
+	axes1.clear()
+	hImshow = axes1.imshow(biscuit, cmap = cm.Greys_r)
 	canvas1.show()
 	toc = time.time()
-	'''data = {'1': {'Time': '{0:.3f}'.format(toc - tic)}}
-	model = table.model
-	model.importDict(data)
-	table.redrawTable()'''
-print 'start'	
+	#~ print toc-tic
+	
+video = cv2.VideoCapture()
 guiMain = Tk()
 guiMain.geometry('850x7000')
-guiMain.title('biscuit_inspection') #how to set full screen
-print 'gui created'
-#Capturing video frame
+guiMain.title('biscuit_inspection')
+
 if not 'video' in locals():
 	video = cv2.VideoCapture()
-while not video.isOpened():
-	print 'attempting open video'
+if not video.isOpened():
 	video.open(0)
-	time.sleep(1)
-else:
-	print "video already open"
+ret, frame = video.read()
+if ret == False:
+	print 'Check if camera is connected'
+	sys.exit() 
+
 ret, frame = video.read()
 frameRgb = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-print "frame acquired status", ret
 threshold = np.loadtxt('Threshold.txt')
-#Creating figure for showing video frame
+
 videoFigure = Figure(figsize=(5, 5))
 axes1 = videoFigure.add_subplot(111)
 videoFigure.suptitle("Live Video")
 hImshow = axes1.imshow(frameRgb, cmap = cm.Greys)
-#Creating canvass for placing videou frame
 canvas1 = FigureCanvasTkAgg(videoFigure, master=guiMain)
 canvas1.get_tk_widget().place(x=10, y=20)
-# table creation
-'''frameValue = Frame(guiMain)
-frameValue.pack()
-frameValue.place(x=100, y=500,width=300, height=70)
-model = TableModel()
-table = TableCanvas(frameValue, model=model, editable=False)
-table.createTableFrame()'''
+
+
 #Timer for frame
 timerFrameDisplay = videoFigure.canvas.new_timer(interval=10)
 timerFrameDisplay.add_callback(showFrame, hImshow)
